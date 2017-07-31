bba2e4de7c03502bb6bd6bddb78d868810fcbb97
Pauline Lalevee <p.lalevee@yahoo.fr>
2017-07-31 17:06:42 +0200

    base java

---
+++ paulinelalv_echecgame/src/Model/Coup.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Paulette
+ */
+public class Coup {
+    //annonce le début et la fin de la partie
+}
